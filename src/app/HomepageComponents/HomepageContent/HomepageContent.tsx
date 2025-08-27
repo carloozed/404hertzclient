16d07b7f0d9a105d23fae7d807113a123c64c6e7
carloozed <carlo@ozelot.ltd>
2025-08-27 15:16:45 +0200

    added login form & button for landing

--- src/app/HomepageComponents/HomepageContent/HomepageContent.tsx
+++ src/app/HomepageComponents/HomepageContent/HomepageContent.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { HomepageDocument } from '../../../../prismicio-types';
 import AnalyzeField from '@/app/components/FormComponents/AnalyzeField/AnalyzeField';
 import Logo from '@/app/components/Logo/Logo';
@@ -9,16 +9,49 @@ import styles from './HomepageContent.module.css';
 import { PrismicRichText } from '@prismicio/react';
 import ButtonWhite from '@/app/components/Buttons/ButtonStyles/ButtonWhite/ButtonWhite';
 import ButtonBlack from '@/app/components/Buttons/ButtonStyles/ButtonBlack/ButtonBlack';
+import LoginForm from '@/app/components/FormComponents/LoginForm/LoginForm';
 
 type HomepageContentProps = {
   page: HomepageDocument;
 };
 
 export default function HomepageContent({ page }: HomepageContentProps) {
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+
   return (
     <div className={styles.mainContainer}>
       <div className={styles.leftContainer}>
-        <div className={styles.catchphrase}>
+        <div
+          className={`${styles.loginContainer} ${isLoginOpen ? styles.loginopen : ''}`}
+        >
+          <div className={styles.loginWrapper}>
+            <div
+              className={styles.crossContainer}
+              onClick={() => setIsLoginOpen(false)}
+            >
+              <div className={styles.cross}>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+            <div>
+              <h2>Welcome back!</h2>
+              <h5>
+                Enter your Login Credentials below or use the options available
+              </h5>
+            </div>
+            <div className={styles.loginFormWrapper}>
+              <LoginForm />
+            </div>
+          </div>
+        </div>
+        <div
+          className={styles.catchphrase}
+          style={{
+            opacity: `${isLoginOpen ? '0' : '1'}`,
+            transition: 'opacity 0.5s ease-in-out',
+          }}
+        >
           <PrismicRichText field={page.data.catchphrase} />
         </div>
         <div className={styles.analyzeFieldContainer}>
@@ -27,10 +60,12 @@ export default function HomepageContent({ page }: HomepageContentProps) {
             <AnalyzeField />
           </div>{' '}
         </div>
-
         <div className={styles.buttonLogoContainer}>
           <div className={styles.buttonContainer}>
-            <ButtonWhite buttonText="Log In" />
+            <ButtonWhite
+              buttonText="Log In"
+              onClick={() => setIsLoginOpen(true)}
+            />
             <ButtonBlack buttonText="Sign Up" />
           </div>
         </div>
